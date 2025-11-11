3978f5623c0f57d9a797bac86349fc9b71b8b142
ogulcansumer-blip <ogulcansumer@gmail.com>
2025-11-11 14:50:03 +0300

    Create page.tsx
    Create minimal page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div style={{ padding: 32, fontSize: 24 }}>
+      ✅ Shumushumu başlangıç sayfası çalışıyor!
+    </div>
+  );
+}
